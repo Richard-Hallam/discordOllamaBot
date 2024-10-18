a9783562ae747473d09edcb719c81cc71323c8b6
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2024-10-18 17:59:39 +0100

    added bot boilerplate js

---
+++ main.js
@@ -0,0 +1,14 @@
+// import the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+//logs in client
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
