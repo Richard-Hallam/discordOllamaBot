87e5606fd978ab4aaba8cf799608c85a2a915231
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2024-10-21 21:17:09 +0100

    fixed ollama call, general code tidy

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-
-
-// Import necessary discord.js classes
-import 'ollama';
 import 'node:fs';
 import ollama from 'ollama' ;
 import token from './config.json' assert {type: 'json'};
-
-let prompt = 'reply with You have to give me a prompt to generate from!';
-
-const chatModel = 'llama3.2:3b';
-
-async function generateResponse(prompt){
-  const result = await ollama.generate({
-    model:chatModel,
-    prompt:prompt,
-    steam:false,
-  })
-  console.log( response.message)
-  return response;
-}
 
 import {Client, Events, GatewayIntentBits, Collection} from 'discord.js';
 import path from 'node:path'
@@ -26,34 +8,33 @@ import { Ollama } from 'ollama';
 import { response } from 'express';
 import { assert } from 'node:console';
 
+let prompt = 'reply with You have to give me a prompt to generate from!';
+
+const chatModel = 'llama3.2:3b';
+
+async function generateResponse(prompt){
+  const result = await ollama.chat({
+    model:chatModel,
+    prompt:prompt,
+    steam:false,
+  })
+  console.log(response.message)
+  return response;
+}
+
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 // Log in client and respond when ready
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-// Log in client and respond when ready
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+})
+
 
 client.on('messageCreate', async (msg) => {
   // if (!msg.content.trim()) return; // Ignore empty messages
 
   // if (msg.author.bot) return; // Ignore bot messages
-client.on('messageCreate', async (msg) => {
-  // if (!msg.content.trim()) return; // Ignore empty messages
-
-  // if (msg.author.bot) return; // Ignore bot messages
-
-  // if (msg.content.toLowerCase().includes('ping')) {
-    console.log(1)
-  //const reply = await generateResponse(msg.content)
-    //console.log(await generateResponse(msg.content))
-    //let replyToDC = msg.reply(generateResponse(prompt));
-    let replyToDC = console.log(await generateResponse(prompt));
-  
-});
-
 
   // if (msg.content.toLowerCase().includes('ping')) {
     console.log(1)
@@ -68,7 +49,3 @@ client.on('messageCreate', async (msg) => {
 
 // Log in to Discord with token
 client.login(token.token);
-client.login(token.token);
-
-//append return questions in the following format before the prompt
-//append return questions in the following format before the prompt
