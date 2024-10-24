0f1713e6efa9892a76fcb52053926262fcb3650b
Richard Hallam <62851758+Richard-Hallam@users.noreply.github.com>
2024-10-24 18:00:16 +0100

    refactored and improved handling of returned content

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import 'node:fs';
 //import ollama from 'ollama' ;
- import token from './config.json' assert {type: 'json'};
+import token from './config.json' assert {type: 'json'};
 import {Client, Events, GatewayIntentBits, Collection} from 'discord.js';
 import path from 'node:path'
 import { Ollama } from 'ollama';
@@ -11,6 +11,30 @@ import { assert } from 'node:console';
 //change this to use a different model
 const chatModel = 'llama3.2:3b';
 
+//initialises ollama
+const ollama = new Ollama({ host: 'http://127.0.0.1:11434' })
+
+//client for discord
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+
+
+/**
+ * Takes a string, then loops over it pushing a slice from the 
+ * start into an array until the string is under 2000 character
+ * @param {string} stringToCheck 
+ * @returns 
+ */
+function splitStringOver2000Characters(stringToCheck){
+  let arrayToReturn = []
+  while (stringToCheck.length > 1999){
+    arrayToReturn.push(stringToCheck.slice(0,1999));
+    let stringToCheck = stringToCheck.slice(2000);
+  }
+  arrayToReturn.push(stringToCheck.slice(0,1999));
+  return arrayToReturn;
+}
+
 
 /**
  * creates ollama instance, sends request and awaits response. Returns assembled response
@@ -19,29 +43,16 @@ const chatModel = 'llama3.2:3b';
  * @returns string
  */
 async function generateResponse(prompt, chatModel){
-  const ollama = new Ollama({ host: 'http://127.0.0.1:11434' })
+  // const ollama = new Ollama({ host: 'http://127.0.0.1:11434' })
   const message = {role: 'user', content: prompt}
 
-  const result = await ollama.chat({ model: chatModel, messages: [message], stream: true })
-  let stringToReturn = ''
-   console.log(await result)
-   const responseParts = [];
-    for await (const part of result) {
-      stringToReturn += part.message.content;
-      responseParts.push(part.message.content);
-   }
-   console.log(responseParts)
-   console.log(stringToReturn)
-   let yeet = stringToReturn;
-   return yeet;
+  
 
+  const result = await ollama.chat({ model: chatModel, messages: [message], stream: false })
+  let stringToReturn = '';
+   console.log(result);
+   return result.message.content;
 }
-
-
-
-//client for discord
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-
 
 /**
  * listens for discord message then passes message content to generateResponse()
@@ -51,13 +62,13 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", msg => {
-  console.log(msg)
   if (msg.content === ''){
     msg.reply('empty message')
   }else{
     if (msg.author.bot){ 
     } else{
-    generateResponse(msg.content, chatModel).then(dcMessage=>(msg.reply(dcMessage)))
+
+    generateResponse(msg.content, chatModel).then(returnString => (msg.reply('reply:' + returnString)));
 }}})
 
 
